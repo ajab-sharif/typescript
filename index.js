dd88de3fa0cc11a974399a1e56bde25939afbc67
sharif <sharif@ajab.sharifulislam.com>
2024-02-12 22:26:56 +0300

    learing functions

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function createUser(_a) {
+    var string = _a.name, string = _a.email;
+    // Example: Store user data in a database
+    console.log("Creating user: ".concat(name, " with email: ").concat(email));
+    // Implement database interaction here
+}
+createUser({ name: "sharif", email: "sharif@gmail.com" });
+//////////////////////////
+/*
+
+
+
+
+
+
+*/
+console.log("TypeScript -->");
--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-console.log("TypeScript -->");
+// functions
+// parameters type
+function greet(person: string = "stranger") {
+  return `hi there, ${person}`;
+}
+greet("shariful islam");
+greet(); //default
+
+const doSometing = (person: string, age: number, isFunny) => {};
+doSometing("sharif", 20, true);
+/////////////////////////////////////////////////////////////
+// functions return type
+const square = function (num: number): number {
+  return num * num;
+};
+square(3);
+function random(num: number) {
+  if (num > 10) {
+    return num.toString();
+  }
+  return num;
+}
+random(12);
+// anonymous functions
+const number = [1, 3, 5];
+const colors = ["red", "green", "orange"];
+
+colors.map((color) => {
+  color.toUpperCase();
+});
+// void type
+function prientTwice(msg: string): void {
+  console.log(msg);
+}
+// never type
+function giveError(msg: string): never {
+  throw new Error("msg");
+}
+export {};
